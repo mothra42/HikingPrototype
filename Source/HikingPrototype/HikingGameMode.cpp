a69901f5fb423812befd6278e1a90115654623e4
Adam <adam.a.farmer4129@gmail.com>
2021-09-16 17:22:18 -0700

    rename some auto generated file names

---
+++ Source/HikingPrototype/HikingGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HikingGameMode.h"
+#include "Hiker.h"
+#include "UObject/ConstructorHelpers.h"
+
+AHikingGameMode::AHikingGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Player/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
