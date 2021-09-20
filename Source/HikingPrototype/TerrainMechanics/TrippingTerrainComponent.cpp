2ede7d1b29ccebc57718299cdb6366e0660e6b4f
Adam <adam.a.farmer4129@gmail.com>
2021-09-20 15:36:53 -0700

    Adds a class to facilitate tripable terrain
    Adds some needed variables and pseudocode for how methods will run.

---
+++ Source/HikingPrototype/TerrainMechanics/TrippingTerrainComponent.cpp
@@ -0,0 +1,32 @@
+// Copyright Adam Farmer 2021
+
+
+#include "TrippingTerrainComponent.h"
+
+// Sets default values for this component's properties
+UTrippingTerrainComponent::UTrippingTerrainComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+
+// Called when the game starts
+void UTrippingTerrainComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+//called from Player's Character Force Component
+bool UTrippingTerrainComponent::bPlayerShouldTrip(float CurrentHikerSpeed)
+{
+	//Calculate if the player should trip based on Trip Probability and other factors
+	//Other factors should be current speed. The angle the hiker is currently on.
+	//whether they are accelerating to become faster or slower.
+	//returns a simple boolean if the player trips or not.
+
+	return false;
+}
+
---
+++ Source/HikingPrototype/TerrainMechanics/TrippingTerrainComponent.h
@@ -0,0 +1,34 @@
+// Copyright Adam Farmer 2021
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "TrippingTerrainComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class HIKINGPROTOTYPE_API UTrippingTerrainComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UTrippingTerrainComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+//protected variables
+protected:
+	//Probability to Trip Player.
+	UPROPERTY(Category = "Trip Probability", EditAnywhere, BlueprintReadOnly)
+	float TripProbability = 0.01f;
+public:	
+	//function that is called from the Hiker's Character Force Component to check if the player should trip.
+	//should scale with the player's current speed. If they are going faster then likelihood of tripping is higher.
+	bool bPlayerShouldTrip(float CurrentHikerSpeed);
+
+		
+};
