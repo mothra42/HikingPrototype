fe2ad7a5316ef6a1793b772a55f13d5b3031aceb
Adam <adam.a.farmer4129@gmail.com>
2021-09-22 18:11:01 -0700

    Updates file path to default pawn

--- Source/HikingPrototype/HikingGameMode.cpp
+++ Source/HikingPrototype/HikingGameMode.cpp
@@ -7,7 +7,7 @@
 AHikingGameMode::AHikingGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Player/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Player/Blueprints/Hiker_BP"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
